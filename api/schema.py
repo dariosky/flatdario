d59766bc3ca9850e52c281c4974fffec9bf6cde0
Dario Varotto <dariosky@gmail.com>
2018-03-12 01:41:03 +0100

    GraphQL schema exact dynamic filtering

--- api/schema.py
+++ api/schema.py
@@ -1,9 +1,7 @@
-from time import sleep
-
 import graphene
-from graphene import relay, Argument, List
+from graphene import relay, Argument, List, NonNull
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from sqlalchemy import inspect, desc
+from sqlalchemy import inspect
 
 from storage.sql import Item
 
@@ -31,31 +29,53 @@ def sort_enum_for(cls):
     return graphene.Enum(name, items)
 
 
+class FilterableConnectionField(SQLAlchemyConnectionField):
+    """ An enhanced ConnectionField that adds 'sort' by any field
+        and arguments in the SqlAlchemy columns for filtering
+    """
+
+    # see discussion on https://github.com/graphql-python/graphene-sqlalchemy/issues/27
+    def __init__(self, type, *args, **kwargs):
+        def set_field_case(field_name):
+            if field_name in {'type', 'sort'}:
+                return field_name.upper()
+            return field_name
+
+        def get_type(field):
+            result = field._type
+            if isinstance(result, NonNull):
+                result = result._of_type
+            return Argument(result)
+
+        filter_fields = {
+            set_field_case(field_name): get_type(field_type)
+            for field_name, field_type in type._meta.fields.items()
+        }
+        super().__init__(type, *args,
+                         **filter_fields,
+                         sort=Argument(List(sort_enum_for(Item))),
+                         **kwargs)
+
+    # See: https://github.com/graphql-python/graphene-sqlalchemy/issues/27#issuecomment-341824371
+    RELAY_ARGS = ['first', 'last', 'before', 'after']
+
+    @classmethod
+    def get_query(cls, model, info, **kwargs):
+        sort = kwargs.pop('sort', None)
+        query = super().get_query(model, info, **kwargs)
+
+        for field, value in kwargs.items():
+            if field not in FilterableConnectionField.RELAY_ARGS:
+                query = query.filter(getattr(model, field.lower()) == value)
+
+        if sort:
+            query = query.order_by(*sort)
+        return query
+
+
 class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-    items = SQLAlchemyConnectionField(ItemType,
-                                      sort=Argument(List(sort_enum_for(Item))))
+    items = FilterableConnectionField(ItemType)
     item = relay.Node.Field(ItemType)
-
-    search = graphene.List(ItemType, q=graphene.String())
-
-    def resolve_search(self, info, q):
-        item_query = ItemType.get_query(info)
-        return item_query.filter(
-            Item.type.contains(q)
-            | Item.title.contains(q)
-            # | Item.extra.contains(q)
-        ).all()
-
-    # if we don't want to use the ConnectionField but a list
-    # we have to implement pagination by ourselves:
-
-    def resolve_items(self, info, *args, sort=None, **kwargs, ):
-        if sort is None:
-            sort = [desc(Item.timestamp), ]
-        query = ItemType.get_query(info)
-        # query = query.filter(Item.type == 'Tumblr')
-        return query.order_by(*sort)
 
 
 schema = graphene.Schema(query=Query)
